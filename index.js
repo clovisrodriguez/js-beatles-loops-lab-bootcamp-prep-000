5fd69a1dd252e048dcdb2ce369b7dea27adb42c0
Clovis Rodríguez <clovis1992@gmail.com>
2018-01-23 15:45:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay (musicians, instruments){
+  let newArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+}
+
+function johnLennonFacts (facts) {
+  while (facts) {
+    
+  }
+}
